034e66a2c773e667df9f37ea49d4432d311e0449
muxi <tlbfpx@163.com>
2026-07-22 15:40:11 +0800

    feat(venue): 6 Mapper

---
+++ hey-pickler-server/src/main/java/com/heypickler/mapper/CourtPricingBandMapper.java
@@ -0,0 +1,6 @@
+package com.heypickler.mapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.heypickler.entity.CourtPricingBand;
+import org.apache.ibatis.annotations.Mapper;
+@Mapper
+public interface CourtPricingBandMapper extends BaseMapper<CourtPricingBand> {}
---
+++ hey-pickler-server/src/main/java/com/heypickler/mapper/VenueContactMapper.java
@@ -0,0 +1,6 @@
+package com.heypickler.mapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.heypickler.entity.VenueContact;
+import org.apache.ibatis.annotations.Mapper;
+@Mapper
+public interface VenueContactMapper extends BaseMapper<VenueContact> {}
